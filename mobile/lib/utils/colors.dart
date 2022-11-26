d27a151f3295d1c1811236efb7afc993205bad7e
Lal Joncoux <lal.joncoux-aydin@epitech.eu>
2022-11-26 11:18:46 +0100

    [FIX] Main color

--- mobile/lib/utils/colors.dart
+++ mobile/lib/utils/colors.dart
@@ -4,4 +4,4 @@ const Color backgroundColor = Color.fromARGB(255, 56, 56, 56);
 const Color primaryColor = Color.fromARGB(255, 255, 255, 255);
 const Color secondaryColor = Color.fromRGBO(175, 174, 174, 1.0);
 const Color thirdColor = Color.fromARGB(255, 58, 58, 58);
-const Color mainColor = Color.fromARGB(255, 243, 12, 104);
+const Color mainColor = Color.fromARGB(255, 30, 105, 189);
